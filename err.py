b59c3bc372839903b235dbbb446ba77847ce68a6
Edward Delmonico <ed.delmonico@gmail.com>
2025-01-26 14:32:27 +0200

    #7 get err.py working

--- err.py
+++ err.py
@@ -1,5 +1,34 @@
-def handle_errors(func):
+d = {"and": "&", "AND": "&",
+     "or": "|", "OR": "|",
+     "not": "1 -", "NOT": "1 -",
+     "(": "(", ")": ")"}          # operator replacements
+
+def list_problem_terms(problem_terms):
+    print("I'm afraid we've found no results for the following:")
+    for problem_term in problem_terms:
+        print(f"---{problem_term}")
+
+
+
+def handle_errors(search, terms):
     try:
-        print(func())
+        search(terms)
     except:
-        print('wuh woh')
+        print(f"There's a problem with your query '{terms}'!")
+        all_terms = terms.split()
+        problem_terms = []
+        prev = ''
+        if len(all_terms) > 1:
+            for term in all_terms:
+                if term not in list(d.keys()):
+                    try:
+                        if prev.lower() == 'not':
+
+                            term = prev + " " + term
+                        print(f"Let's try searching for {term}")
+                        search(term)
+                    except:
+                        problem_terms.append(term)
+                prev = term
+        else: problem_terms.append(terms)
+        list_problem_terms(problem_terms)
