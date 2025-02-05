2111d7e3472bd00674d083c1d8972e70c3a7961a
Edward Delmonico <ed.delmonico@gmail.com>
2025-02-05 15:48:40 +0200

    generalize into function

---
+++ bookDescription.py
@@ -0,0 +1,31 @@
+import requests
+from bs4 import BeautifulSoup
+import re
+from selenium import webdriver
+
+def cleanup(str):
+    str = re.sub('\[\^\]', '', str)
+ #   str = re.sub('([A-Za-z]+)([A-Z]+)', '\1. \2', str)
+    return str
+
+def get(book_url):
+
+    session = requests.Session()
+    r = session.get(book_url)
+
+    options = webdriver.ChromeOptions()
+    options.add_argument("--headless")
+
+    user_agent = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.116 Safari/537.36'
+    options.add_argument(f'user-agent={user_agent}')
+
+    dr = webdriver.Chrome(
+        options=options
+        )
+    dr.get(book_url)
+
+    soup = BeautifulSoup(dr.page_source, 'html.parser')
+    details = soup.find_all('div', class_ = 'details')
+
+    return cleanup(details[1].text.strip())
+
