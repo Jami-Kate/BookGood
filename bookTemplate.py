82a958bc81809877368784ed5c0b0bb7f4058a93
Edward Delmonico <ed.delmonico@gmail.com>
2025-02-05 11:08:00 +0200

    can now get book descriptions bay bee

--- bookTemplate.py
+++ bookTemplate.py
@@ -1,20 +1,34 @@
-#from requests_html import HTMLSession
-import lxml_html_clean
-
 import requests
 from bs4 import BeautifulSoup
 import re
 from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
 
-url = 'https://www.whsmith.co.uk/Product/Gretchen-Felker-Martin/Cuckoo/13100619?_gl=1*1a671yr*_up*MQ..*_ga*Nzg3ODUxNzAwLjE3MzgzMjI2Nzk.*_ga_9YDXQFM2GM*MTczODMyMjY3OC4xLjAuMTczODMyMjY3OC4wLjAuMTA1NDI3NTE3Ng..'
+def cleanup(str):
+    str = re.sub('\[\^\]', '', str)
+    return str
+
+
+cuckoo = 'https://www.whsmith.co.uk/Product/Gretchen-Felker-Martin/Cuckoo/13100619?_gl=1*1a671yr*_up*MQ..*_ga*Nzg3ODUxNzAwLjE3MzgzMjI2Nzk.*_ga_9YDXQFM2GM*MTczODMyMjY3OC4xLjAuMTczODMyMjY3OC4wLjAuMTA1NDI3NTE3Ng..'
+pote = 'https://www.whsmith.co.uk/Product/Ken-Follett/The-Pillars-of-the-Earth/11576358?_gl=1*1nq3jo4*_up*MQ..*_ga*MjA2OTY3ODg1NC4xNzM4NzQ1Nzc0*_ga_9YDXQFM2GM*MTczODc0NTc3My4xLjAuMTczODc0NTc3My4wLjAuNDk1OTc2NjM1'
 
 session = requests.Session()
-r = session.get(url)
+r = session.get(pote)
 
+options = webdriver.ChromeOptions()
+options.add_argument("--headless")
 
-dr = webdriver.Chrome()
-dr.get(url)
+user_agent = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.116 Safari/537.36'
+options.add_argument(f'user-agent={user_agent}')
+
+dr = webdriver.Chrome(
+    options=options
+    )
+dr.get(pote)
 
 soup = BeautifulSoup(dr.page_source, 'html.parser')
-print(soup.find('div', class_ = 'summary'))
+description = soup.find('div', class_ = 'summary')
+details = soup.find_all('div', class_ = 'details')
+
+print(cleanup(details[1].text.strip()))
 
