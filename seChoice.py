8b3b4a95a5d2aecf0057f1b65dec0943919dfe91
SonjaTapio <sonja.tapio@helsinki.fi>
2025-02-06 16:50:08 +0200

    Made seChoice.py prettier

--- seChoice.py
+++ seChoice.py
@@ -1,14 +1,13 @@
-seChoice = input("What search engine would you like to use today?\n 1. Boolean\n 2. Tf-idf\n 3. Actually, I'd rather not\n")
+options = {"1": "searchEngine.py", "2": "tfidfSearchEngine.py"}
 
-if seChoice == "1":
-    filename = 'searchEngine.py'
-    with open(filename) as file:
-        exec(file.read())
-elif seChoice == "2":
-    filename = 'tfidfSearchEngine.py'
-    with open(filename) as file:
-        exec(file.read())
-elif seChoice == "3": 
-    print("See you later!")
-else:
-    print("Please only type 1, 2, 3 as your answer")
+while True:
+    seChoice = input("What search engine would you like to use today?\n 1. Boolean\n 2. Tf-idf\n 3. Actually, I'd rather not\n")
+    
+    if seChoice in options:
+        with open(options[seChoice]) as file:
+            exec(file.read())
+    elif seChoice == "3":
+        print("See you later!")
+        break
+    else:
+        print("Please only type 1, 2, or 3 as your answer")
