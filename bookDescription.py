19cdf1d5990a0a40f2086f0c4bcc21fac5ae2d16
Edward Delmonico <ed.delmonico@gmail.com>
2025-02-06 15:20:12 +0200

    generalize back to function

--- bookDescription.py
+++ bookDescription.py
@@ -11,35 +11,33 @@ pote = 'https://www.whsmith.co.uk/Product/Ken-Follett/The-Pillars-of-the-Earth/1
 
 cuckoo = 'https://www.whsmith.co.uk/Product/Gretchen-Felker-Martin/Cuckoo/13100619?_gl=1*1a671yr*_up*MQ..*_ga*Nzg3ODUxNzAwLjE3MzgzMjI2Nzk.*_ga_9YDXQFM2GM*MTczODMyMjY3OC4xLjAuMTczODMyMjY3OC4wLjAuMTA1NDI3NTE3Ng'
 
-book_url = pote
+def get_book(book_url):
 
-# def get(book_url):
+    session = requests.Session()
+    r = session.get(book_url)
 
-session = requests.Session()
-r = session.get(book_url)
+    options = webdriver.ChromeOptions()
+    options.add_argument("--headless")
 
-options = webdriver.ChromeOptions()
-options.add_argument("--headless")
+    user_agent = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.116 Safari/537.36'
+    options.add_argument(f'user-agent={user_agent}')
 
-user_agent = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.116 Safari/537.36'
-options.add_argument(f'user-agent={user_agent}')
+    dr = webdriver.Chrome(
+        options=options
+        )
+    dr.get(book_url)
 
-dr = webdriver.Chrome(
-    options=options
-    )
-dr.get(book_url)
+    soup = BeautifulSoup(dr.page_source, 'html.parser')
+    title = soup.find('h1', itemprop = 'name').contents[0].text.strip()
+    author = soup.find('span', itemprop = 'name').contents[0].text.strip()
+    category = soup.find('ul', class_ = 'breadcrumb__list').contents[-1].text.strip()
+    details = soup.find_all('div', class_ = 'details')[1].text.strip()
 
-soup = BeautifulSoup(dr.page_source, 'html.parser')
-title = soup.find('h1', itemprop = 'name').contents[0].text.strip()
-author = soup.find('span', itemprop = 'name').contents[0].text.strip()
-category = soup.find('ul', class_ = 'breadcrumb__list').contents[-1].text.strip()
-details = soup.find_all('div', class_ = 'details')[1].text.strip()
+    return {'title' : title, 'author' : author, 'category' : category, 'details' : cleanup(details)}
 
-print(title)
-print(author)
-print(category)
-print(cleanup(details))
+test = get_book(pote)
 
-#     return cleanup(details[1].text.strip())
-
-# get(cuckoo)
+print(test['title'])
+print(test['author'])
+print(test['category'])
+print(test['details'])
