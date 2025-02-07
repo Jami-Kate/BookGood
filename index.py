5066354eaf0462e61cd3936bad7e85481b385f43
Edward Delmonico <ed.delmonico@gmail.com>
2025-02-07 13:10:42 +0200

    create GUI branch

--- index.py
+++ index.py
@@ -2,6 +2,18 @@ from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/') # Gets you to homepage
 def home():
     return render_template('index.html')
+
+@app.route('/search') # Searches term from form
+def search():
+    query = request.args.get('query')
+    return query
+
+@app.route('/results/<query>')
+def display_results(query):
+    return render_template('results.html', query = query)
+
+if __name__ == "__main__":
+    app.run(debug=True)
