b32780330d3089ccd9b18268ffdf9209e9f99e7f
Edward Delmonico <ed.delmonico@gmail.com>
2025-02-18 12:00:20 +0200

    clean up bookmood.py

--- engine/bookMood.py
+++ engine/bookMood.py
@@ -1,23 +1,6 @@
-from transformers import pipeline
-
 import matplotlib.pyplot as plt
 
-# import json
-
 from math import pi
-
-# with open('./data/data.json','r') as f:
-#     books = json.load(f)
-
-
-# test_review = books[0]['review']
-# print(test_review)
-
-# # Runs classifier on selected book review to extract list of moods
-# classifier = pipeline(task="text-classification", model="SamLowe/roberta-base-go_emotions", top_k=None)
-
-# model_outputs = classifier(test_review)[0]
-# print(model_outputs)
 
 def plot_moods(book, n = 5):
     book = [mood for mood in book if mood['label'] != 'neutral'][:n] # Filter out the 'neutral' label if present and make a list of n most prominent moods (5 by default)
@@ -30,7 +13,7 @@ def plot_moods(book, n = 5):
     angles = [n / float(N) * 2 * pi for n in range(N)]
     angles += angles[:1]
 
-    # Create polar plot
+    # Create radar plot
     ax = plt.subplot(111, polar=True)
 
     plt.xticks(angles[:-1], list(labels), color='grey', size=10)
@@ -46,5 +29,3 @@ def plot_moods(book, n = 5):
     ax.fill(angles, scores, 'b', alpha=0.1)
 
     plt.show()
-
-# plot_moods(model_outputs)
