35025885ac9845c9cacde74e842a8d63139e9ad0
SonjaTapio <sonja.tapio@helsinki.fi>
2025-02-18 14:42:08 +0200

    made the pie chart slightly less ugly

--- engine/plotPie.py
+++ engine/plotPie.py
@@ -13,8 +13,8 @@ def plot_pie(df, sortedIndices):
     for row in genres:
         flatGenres.extend(row) # merge all lists in one 
     count_ = Counter(flatGenres) # count the number of genres 
-    fig, ax = plt.subplots(figsize=(10, 7))
-    ax.pie(count_.values(), labels=count_.keys(), startangle = 90, colors=sns.color_palette('Set2'))
+    fig, ax = plt.subplots(figsize=(10, 9), dpi=400) # dpi: increase resolution
+    ax.pie(count_.values(), labels=count_.keys(), startangle = 90, colors=sns.color_palette('Set2'), textprops={'fontsize': 15})  # Reduce text size)
     img = io.BytesIO()
     
     return fig, img 
