55aa9f297436a36b78bfcc0fd2b5d7d05229cd9f
Edward Delmonico <ed.delmonico@gmail.com>
2025-02-21 12:22:09 +0200

    add about page and route

--- app.py
+++ app.py
@@ -57,6 +57,10 @@ def display_book(id):
 
     return render_template('book.html', book = book, mood = mood64)
 
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
 @app.errorhandler(404)
 def redirect(e):
     return render_template('err.html', e = e)
